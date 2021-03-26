24d74080b8e2816fcb441823f9b4428621968e00
test <leejh275@gmail.com>
2021-03-26 12:02:29 +0900

    [21.03.26] Spring 학습 5일차

--- README.md
+++ README.md
@@ -4,10 +4,11 @@
 2. src/main/resources폴더로 가서 application.properties 파일에 'server.port = 80'으로 수정
 3. src/main/java에 MvcApplication클래서 main메서드 실행해서 서버띄우기
 
-## JSP 팡리 템플릿
+## JSP 파일 템플릿
 ```jsp
 
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
 <!doctype html>
 <html lang="ko">
--- src/main/java/com/spring/mvc/basic/RequestServlet.java
+++ src/main/java/com/spring/mvc/basic/RequestServlet.java
@@ -1,5 +1,8 @@
 package com.spring.mvc.basic;
 
+import com.spring.mvc.user.domain.User;
+import com.spring.mvc.user.repository.MemoryUserRepository;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,15 +11,29 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+// DB에 회원가입을 시켜야 함
 @WebServlet(urlPatterns = "/req-param")
 public class RequestServlet extends HttpServlet {
+
+    private MemoryUserRepository repository = new MemoryUserRepository();
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        // 클라이언트가 준 데이터를 읽음
         String name = req.getParameter("username");
         int age = Integer.parseInt(req.getParameter("age"));
 
+        // 데이터를 객체로 래핑
+        User newUser = new User(name, age);
+
+        // 이 데이터를 DB로 보냄
+        repository.save(newUser);
+
+        // 회원 전체 목록 조회 요청을 해야함. (/show-users)
+        resp.sendRedirect("/show-users");
+
+        /*
         resp.setCharacterEncoding("utf-8");
         resp.setContentType("text/html");
 
@@ -32,6 +49,7 @@ public class RequestServlet extends HttpServlet {
         w.println("</div>");
         w.println("</body>");
         w.println("</html>");
+        */
 
     }
 }
